5a94210ef77654a1b330a2479170319a8d1c69ce
salintempel <el.pengunista@gmail.com>
2021-03-22 18:04:52 +0700

    akses dashboard tanpa login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 // import './App.css';
-import Login from './Login'
+import Login from './Login';
+import Dashboard from './Dashboard'
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+  const [isLogin, setLogin] = useState(false)
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        {!isLogin && <Login />}
+      </div>
+      {/* just for temporary before Login work */}
+      <Link to="/dashboard" onClick={() => setLogin(true)}>masuk tanpa login</Link>
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Dashboard from './Dashboard'
 
 export default function Login() {
     return (
+
         <div className="Login" >
             <form action="" method="post" className="form-login">
-
                 <input placeholder="email" type="email" name="email" id="email" autocomplete="off" />
-
                 <input placeholder="password" type="password" name="password" id="password" autocomplete="off" />
                 <button type="submit" onClick="" className="btn-login">Login</button>
             </form>
-            <p>masuk tanpa login</p>
+
         </div>
+
     )
 }
